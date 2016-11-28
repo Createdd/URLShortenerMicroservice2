ed4325264dbfb159dfd21cf2f189f69234455fe4
Daniel Deutsch <deudan1010@gmail.com>
2016-11-28 21:18:17 +0100

    utilizing req parameters properly

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var mongodb=require("mongodb");
+var shortid=require("shortid");//for generating short urls
+var validUrl=require("valid-url");//to verify the url
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/new/:url(*)', function(req, res, next) {
+  res.send(req.params.url);
+  //res.render('index', { title: 'Express' });
+});//the * allows proper formatting/ alternative would be regEx
 
 module.exports = router;
