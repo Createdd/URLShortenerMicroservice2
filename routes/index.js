a79b955a902c1c5f068a94b314eeb4f31f091b9a
Daniel Deutsch <deudan1010@gmail.com>
2016-11-28 21:26:33 +0100

    connecting to mongoDB successfully

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
+/* jshint esversion:6*/
+
 var express = require('express');
 var router = express.Router();
 var mongodb=require("mongodb");
+var mLab="mongodb://test:test@ds159387.mlab.com:59387/urlshortener";//store mlab url
+var MongoClient=mongodb.MongoClient;//to host mongoDB connect command
 var shortid=require("shortid");//for generating short urls
 var validUrl=require("valid-url");//to verify the url
 
 /* GET home page. */
 router.get('/new/:url(*)', function(req, res, next) {
-  res.send(req.params.url);
+  MongoClient.connect(mLab,(err,db)=>{
+    if(err){
+      console.log("No connection to MongoDB", err);
+    } else{
+      console.log("Connected to MongoDB");
+    }
+  });//connect to mongoDB
   //res.render('index', { title: 'Express' });
 });//the * allows proper formatting/ alternative would be regEx
 
