6cdd2bc044d7fce61a63376cc570e8e56edf10e7
Daniel Deutsch <deudan1010@gmail.com>
2016-11-29 11:08:46 +0100

    check url and generate short url success

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ var MongoClient=mongodb.MongoClient;//to host mongoDB connect command
 var shortid=require("shortid");//for generating short urls
 var validUrl=require("valid-url");//to verify the url
 
-/* GET home page. */
-router.get('/new/:url(*)', function(req, res, next) {
+
+router.get('/new/:url(*)', (req, res, next)=>{
   MongoClient.connect(mLab,(err,db)=>{
     if(err){
       console.log("No connection to MongoDB", err);
@@ -18,16 +18,24 @@ router.get('/new/:url(*)', function(req, res, next) {
       var collection=db.collection("links");//create a db collection
       var params=req.params.url;//set the url as parameter
       var newLink=(db,callback)=>{
-        var insertLink={url:params, short:"test"};//create a new object
+        /*var insertLink={url:params, short:"test"};//create a new object
         collection.insert([insertLink]);//store the object in the database
-        res.send(params);//output the url
+        res.send(params);//output the url*/
+        if(validUrl.isUri(params)){
+          var shortCode=shortid.generate();
+          var newUrl={url:params,short:shortCode};
+          collection.insert([newUrl]);
+          res.json({original_url:params, short_url: "localhost:3000/"+shortCode});
+          console.log(validUrl.isUri(params));
+        }else{
+          res.json({error: "URL is not valid"});
+        }
       };//function to import a link to the database and returns a short link
       newLink(db,()=>{
         db.close();
       });//accept a callback function and close the DB
     }
   });//connect to mongoDB
-  //res.render('index', { title: 'Express' });
 });//the * allows proper formatting/ alternative would be regEx
 
 module.exports = router;
